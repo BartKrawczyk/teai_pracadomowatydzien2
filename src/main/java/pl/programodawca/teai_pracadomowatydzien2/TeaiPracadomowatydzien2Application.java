b293db0762cbcf09b9d5b1456bb184ce475d0f52
Bartłomiej Krawczyk <bkrawczyk@pkmtychy.local>
2022-02-02 11:10:32 +0100

    Initial commit

---
+++ src/main/java/pl/programodawca/teai_pracadomowatydzien2/TeaiPracadomowatydzien2Application.java
@@ -0,0 +1,13 @@
+package pl.programodawca.teai_pracadomowatydzien2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeaiPracadomowatydzien2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeaiPracadomowatydzien2Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/programodawca/teai_pracadomowatydzien2/TeaiPracadomowatydzien2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.programodawca.teai_pracadomowatydzien2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeaiPracadomowatydzien2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
